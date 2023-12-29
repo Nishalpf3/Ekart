f80b5d42e579f855c4f44e0c1068c511e70f07a2
MUHAMMED NSHAL P.F <107340672+Nishalpf3@users.noreply.github.com>
2023-12-30 02:20:06 +0530

    order and payment implimented

--- Gkart/accounts/views.py
+++ Gkart/accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from .forms import RegistrationForm
 from .models import Account
-from django.contrib import messages,auth
+from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
